2f585f2e0130460968a59727374808b9981f5d68
Pavle Sekesan <pavlesekesan@gmail.com>
2023-05-04 23:42:58 +0200

    get_problem_name and refactoring

--- problem.py
+++ problem.py
@@ -3,36 +3,42 @@ from urls import PETLJA_URL
 from bs4 import BeautifulSoup
 
 
-def get_problem_id(session, uniqueid):
-    problems_page = session.get(f"{PETLJA_URL}/problems/{uniqueid}")
-    soup = BeautifulSoup(problems_page.text, "html.parser")
+def get_problem_id(session, alias):
+    page = session.get(f"{PETLJA_URL}/problems/{alias}")
+    soup = BeautifulSoup(page.text, "html.parser")
     problem_id = soup.find("button", attrs={"class": "btn-solution-submit"})[
         "data-problem-id"
     ]
     return problem_id
 
 
-def create_problem(session, name, uniqueid):
+def get_problem_name(session, problem_id):
+    page = session.get(f"{PETLJA_URL}/cpanel/EditProblem/{problem_id}")
+    soup = BeautifulSoup(page.text, "html.parser")
+    problem_name = soup.find("input", attrs={"id": "Problem_Name"})["value"]
+    return problem_name
+
+
+def create_problem(session, name, alias):
     create_problem_page = session.get(f"{PETLJA_URL}/cpanel/CreateTask")
     csrf_token = get_csrf_token(create_problem_page.text)
-    create_task_resp = session.post(
+    resp = session.post(
         f"{PETLJA_URL}/cpanel/CreateTask",
         data={
             "Name": name,
-            "UniqueId": uniqueid,
+            "UniqueId": alias,
             "Type": "0",
             "__RequestVerificationToken": csrf_token,
         },
     )
+    return get_problem_id(session, alias)
 
 
 def upload_testcases(session, problem_id, testcases_path):
-    upload_testcases_page = session.get(
-        f"{PETLJA_URL}/cpanel/EditProblem/{problem_id}?tab=testcases"
-    )
-    csrf_token = get_csrf_token(upload_testcases_page.text)
+    page = session.get(f"{PETLJA_URL}/cpanel/EditProblem/{problem_id}?tab=testcases")
+    csrf_token = get_csrf_token(page.text)
     with open(testcases_path, "rb") as zip:
-        upload_testcases_resp = session.post(
+        resp = session.post(
             f"{PETLJA_URL}/cpanel/EditProblem/{problem_id}",
             files={"TestCases": zip},
             data={
