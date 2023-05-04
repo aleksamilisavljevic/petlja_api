3b622b034c1c4b60dc5ccacd66d66fb85f231348
Pavle Sekesan <pavlesekesan@gmail.com>
2023-05-05 00:18:04 +0200

    determine language id from submitted file extension

--- submit.py
+++ submit.py
@@ -1,23 +1,30 @@
-from auth import get_session
 from urls import ARENA_URL
 import time
 
+# Weird Petlja API language IDs
+LANGUAGE_IDS = {
+    "c": 10,
+    "cs": 1,
+    "cpp": 2,
+    "java": 3,
+    "m": 7,
+    "pas": 4,
+    "py": 9,
+}
 
-def submit(competition_id, problem_id, source, language_id):
-    session = None
-    while session is None:
-        try:
-            session = get_session()
-        except PermissionError as e:
-            print(e)
 
+def submit(session, competition_id, problem_id, source_path):
+    with open(source_path) as source_file:
+        source = source_file.read()
+
+    extension = source_path.split(".")[-1]
     submit = session.post(
         f"{ARENA_URL}/api/competition/submit-competition-problem",
         json={
             "competitionId": competition_id,
             "problemId": problem_id,
             "source": source,
-            "languageId": language_id,
+            "languageId": LANGUAGE_IDS[extension],
         },
     )
     submission_id = submit.json()["value"]
